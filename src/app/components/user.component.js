7bc354973d68c16475d403331aaeef9b1ba29040
razzcoder <rajatsbhadauria@gmail.com>
2017-03-17 00:29:14 +0530

    Add and Delete Hobby Option Added

--- src/app/components/user.component.js
+++ src/app/components/user.component.js
@@ -18,11 +18,22 @@ var UserComponent = (function () {
             city: 'Lucknow',
             state: 'Uttar Pradesh'
         };
+        this.hobbies = ['Music', 'Movies', 'Sports'];
+        this.showHobbies = false;
     }
+    UserComponent.prototype.toggleHobbies = function () {
+        this.showHobbies = !this.showHobbies;
+    };
+    UserComponent.prototype.addHobby = function (hobby) {
+        this.hobbies.push(hobby);
+    };
+    UserComponent.prototype.deleteHobby = function (i) {
+        this.hobbies.splice(i, 1);
+    };
     UserComponent = __decorate([
         core_1.Component({
             selector: 'user',
-            template: "\n  <h1>Hello {{name}}</h1>\n  <p><strong>Email </strong> : {{email}}</p>\n  <p><strong>Address </strong>: {{address.city}} {{address.state}} {{address.street}}</p>\n  ",
+            template: "\n  <h1>{{name}}</h1>\n  <p><strong>Email </strong> : {{email}}</p>\n  <p><strong>Address </strong>: {{address.city}} {{address.state}} {{address.street}}</p>\n  <button (click)=\"toggleHobbies()\">{{showHobbies ? \"Hide Hobbies\" : \"Show hobbies\"}}</button>\n  <div *ngIf=\"showHobbies\">\n  <h3>Hobbies</h3>\n\n  <ul>\n  \t<li *ngFor=\"let hobby of hobbies;let i=index\">\n  \t\t{{ hobby }} <button (click)=\"deleteHobby(i)\">X</button>\n  \t</li>\n  </ul>\n  <form (submit)=\"addHobby(hobby.value)\">\n  \t<label> Add Hobby : </label><br/>\n  \t<input type=\"text\" #hobby /><br />\n  </form>\n\n\n  </div>\n  <hr />\n  <h3>Edit User </h3>\n  <form>\n  \t<label>Name : </label>\n  \t<input type=\"text\" name=\"name\" [(ngModel)]=\"name\" /><br/>\n  \t<label>Email : </label>\n  \t<input type=\"text\" name=\"email\" [(ngModel)]=\"email\" /><br/>\n  \t<label>Street : </label>\n  \t<input type=\"text\" name=\"street\" [(ngModel)]=\"address.street\" />\n  \t<br/>\n  \t<label>City : </label>\n  \t<input type=\"text\" name=\"city\" [(ngModel)]=\"address.city\" />\n  \t<br/>\n  \t<label>State : </label>\n  \t<input type=\"text\" name=\"state\" [(ngModel)]=\"address.state\" />\n  \t<br/>\n  </form>\n  ",
         }), 
         __metadata('design:paramtypes', [])
     ], UserComponent);
--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -3,9 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'user',
   template: `
-  <h1>Hello {{name}}</h1>
+  <h1>{{name}}</h1>
   <p><strong>Email </strong> : {{email}}</p>
   <p><strong>Address </strong>: {{address.city}} {{address.state}} {{address.street}}</p>
+  <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show hobbies"}}</button>
+  <div *ngIf="showHobbies">
+  <h3>Hobbies</h3>
+
+  <ul>
+  	<li *ngFor="let hobby of hobbies;let i=index">
+  		{{ hobby }} <button (click)="deleteHobby(i)">X</button>
+  	</li>
+  </ul>
+  <form (submit)="addHobby(hobby.value)">
+  	<label> Add Hobby : </label><br/>
+  	<input type="text" #hobby /><br />
+  </form>
+
+
+  </div>
+  <hr />
+  <h3>Edit User </h3>
+  <form>
+  	<label>Name : </label>
+  	<input type="text" name="name" [(ngModel)]="name" /><br/>
+  	<label>Email : </label>
+  	<input type="text" name="email" [(ngModel)]="email" /><br/>
+  	<label>Street : </label>
+  	<input type="text" name="street" [(ngModel)]="address.street" />
+  	<br/>
+  	<label>City : </label>
+  	<input type="text" name="city" [(ngModel)]="address.city" />
+  	<br/>
+  	<label>State : </label>
+  	<input type="text" name="state" [(ngModel)]="address.state" />
+  	<br/>
+  </form>
   `,
 })
 
@@ -13,6 +46,8 @@ export class UserComponent  {
 	name:string;
 	email:string;
 	address:address;
+	hobbies:string[];
+	showHobbies:boolean;
 
 	constructor(){
 		this.name='John Doe';
@@ -23,6 +58,21 @@ export class UserComponent  {
 			city:'Lucknow',
 			state:'Uttar Pradesh'
 		}
+		this.hobbies=['Music','Movies','Sports'];
+		this.showHobbies=false;
+	}
+	toggleHobbies(){
+		this.showHobbies=!this.showHobbies;
+
+
+	}
+	addHobby(hobby){
+
+		this.hobbies.push(hobby);
+	}
+	deleteHobby(i){
+		this.hobbies.splice(i,1);
+
 	}
 }
 
